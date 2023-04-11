302f4a22193fdd426ed9e29e86fe3479d53f4285
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-11 17:12:55 +0200

    FEAT basic style cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,23 @@
         <h3>questo e' il contenuto tramite controller</h3>
 
         @forelse ($movies as $movie)
-            <div class="col-6 pt-5">
+        <div class="card my-4" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Titolo Ita: {{ $movie->title }}</h5>
+              <h5 class="card-title">Titolo Originale: <br>{{ $movie->original_title }}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Lingua originale: {{ $movie->nationality }}</li>
+              <li class="list-group-item">Release {{ $movie->date }}</li>
+            </ul>
+          </div>
+            {{-- <div class="col-6 pt-5">
                 <h3>Titolo Ita: {{ $movie->title }}</h3>
                 <h3>Titolo Originale: {{ $movie->original_title }}</h3>
                 <p>Nazionalita' film: {{ $movie->nationality }}</p>
                 <p>Data release: {{ $movie->date }} </p>
-            </div>
+            </div> --}}
         @empty
 
         @endforelse
