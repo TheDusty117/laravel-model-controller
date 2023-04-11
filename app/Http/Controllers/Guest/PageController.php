1e97734d070b5bd82492868cbfbf10408e1530ae
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-11 17:01:09 +0200

    FEAT data from movies added in pagectrlr

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+
+//aggiungi questo per i model
+use App\Models\Movie;
+
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function homepage()
     {
-        return view('welcome');
+
+        //qui vado ad utilizzare il database dei Movies tramite i Models
+        //qui dico dammi tutti i film
+        $movies = Movie::all(); //SELECT * FROM movies
+        //provo dump die per vedere cosa mi restituisce
+        // dd($movies);
+
+        $data = [
+            'movies' => $movies
+        ];
+
+        return view('welcome', $data);
+
     }
 }
