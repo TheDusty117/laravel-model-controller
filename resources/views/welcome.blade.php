ee00039d1b4b40ac214bf891e0843d1f2bb489c7
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-11 17:06:38 +0200

    FEAT correct cycle forelse from db movies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,18 @@
 <section class="content-section">
     <div class="container">
         <h3>questo e' il contenuto tramite controller</h3>
+
+        @forelse ($movies as $movie)
+            <div class="col-6 pt-5">
+                <h3>Titolo Ita: {{ $movie->title }}</h3>
+                <h3>Titolo Originale: {{ $movie->original_title }}</h3>
+                <p>Nazionalita' film: {{ $movie->nationality }}</p>
+                <p>Data release: {{ $movie->date }} </p>
+            </div>
+        @empty
+
+        @endforelse
+
     </div>
 </section>
 
