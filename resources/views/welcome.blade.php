8a195948ed79fa053b6bbf3957156aa8bb6850ed
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-11 16:28:17 +0200

    FEAT controller plugged by web.php and Pgcontrolr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 
 <section class="content-section">
     <div class="container">
-        <h3>questo e' il contenuto</h3>
+        <h3>questo e' il contenuto tramite controller</h3>
     </div>
 </section>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+//AGGIUNGI QUESTO
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//AGGIUNGI QUESTO
+Route::get('/', [Pagecontroller::class, 'homepage']);
